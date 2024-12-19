4b6cc9b7a551c92925e787111856c06ae00b7d42
suresh379 <suresh234363@gmail.com>
2024-12-19 11:43:50 +0530

    Add files via upload

---
+++ instancevariable.java
@@ -0,0 +1,17 @@
+package samplejava;
+
+public class instancevariable {
+int a=50;
+int b=2;
+int c=a/b;
+public void divided() {
+
+System.out.println("divided of 50 :"+c);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+instancevariable obj=new instancevariable();
+obj.divided();
+	}
+
+}
