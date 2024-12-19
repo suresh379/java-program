7d3f7ad9f649e53b9544e0937bc6ae6d4af0c9f4
suresh379 <suresh234363@gmail.com>
2024-12-19 11:45:22 +0530

    Add files via upload

---
+++ localvariable5.java
@@ -0,0 +1,17 @@
+package samplejava;
+
+public class localvariable5 {
+ void totalamount() {
+	 int salesman=10000;
+	 int offier=5000;
+	 int total=salesman/offier;
+	 System.out.println("totalamount is :"+total);
+	 
+ }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+localvariable5 obj=new localvariable5();
+obj.totalamount();
+	}
+
+}
