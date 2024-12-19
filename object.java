8edd6b4775d512129b624460a92affa0ed8cc901
suresh379 <suresh234363@gmail.com>
2024-12-19 11:42:49 +0530

    Add files via upload

---
+++ object.java
@@ -0,0 +1,20 @@
+package samplejava;
+
+ public class object {
+    static int a=10;
+    static int b=20;
+    static int c=a+b;
+    static void simpledisplay() {
+    	
+    	System.out.println(c);   
+    	}
+
+
+    public static void main(String args[]) {
+    	object  obj=new object();
+        obj.simpledisplay();
+    
+}
+}
+	 
+	
