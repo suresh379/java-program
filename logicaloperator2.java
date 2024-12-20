eae13733cb358d8d4af51a54d432821679ef291c
suresh379 <suresh234363@gmail.com>
2024-12-20 11:49:09 +0530

    Add files via upload

---
+++ logicaloperator2.java
@@ -0,0 +1,22 @@
+package samplejava;
+
+public class logicaloperator2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+if(a>5 && b>15) {
+	System.out.println("both condition is true .");
+}
+if(a<5 ||b>15) {
+	System.out.println("atleast one condition is true .");
+	
+}
+boolean istrue=false;
+if(!istrue) {
+	System.out.println("condition is false.");
+}
+	}
+
+}
