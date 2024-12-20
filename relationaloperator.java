6ec022c5e27135f6568a8137129690674633cd21
suresh379 <suresh234363@gmail.com>
2024-12-20 11:49:42 +0530

    Add files via upload

---
+++ relationaloperator.java
@@ -0,0 +1,29 @@
+package samplejava;
+
+public class relationaloperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=20,b=10;
+System.out.println("a==b :"+(a==b));
+System.out.println("a!=b :"+(a!=b));
+System.out.println("a>b :"+(a>b));
+System.out.println("a<b :"+(a<b));
+System.out.println("a<=b :"+(a<=b));
+System.out.println("a>=b :"+(a>=b));
+
+
+//bitwise operator
+int c=6;
+int d=7;
+System.out.println("a&b :"+(a&b));
+System.out.println("a|b :"+(a|b));
+System.out.println("a^b :"+(a^b));
+System.out.println("a<<1 :"+(a<<1));
+System.out.println("a>>1 :"+(a>>1));
+System.out.println("a>>>1 :"+(a>>>1));
+
+
+	}
+
+}
