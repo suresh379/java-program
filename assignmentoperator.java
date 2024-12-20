ea40aff4ad38afc8d844d58814c8df0ce621ab00
suresh379 <suresh234363@gmail.com>
2024-12-20 11:50:46 +0530

    Add files via upload

---
+++ assignmentoperator.java
@@ -0,0 +1,14 @@
+package samplejava;
+
+public class assignmentoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=5;
+System.out.println("value of a :"+a);
+a=10;
+System.out.println("update value of a :"+a);
+
+	}
+
+}
