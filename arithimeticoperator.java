1d246e32bc6eba750335ca3bf6ea89212c1eb39c
suresh379 <suresh234363@gmail.com>
2024-12-20 11:52:38 +0530

    Add files via upload

---
+++ arithimeticoperator.java
@@ -0,0 +1,24 @@
+package samplejava;
+
+public class arithimeticoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 5, b = 15;
+//arithimetic operator
+int sum=a+b;
+int variable=a-b;
+int operator=a*b;
+int remained=a/b;
+
+System.out.println("sum is :"+sum);
+System.out.println("variable is :"+variable);
+System.out.println("operator is :"+operator);
+System.out.println("remained is :"+remained);
+
+
+
+
+	}
+
+}
