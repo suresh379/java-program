e2c6249747bcef6675eb4b0118c360f3d504affc
suresh379 <suresh234363@gmail.com>
2024-12-23 12:08:35 +0530

    Add files via upload

---
+++ oddandeven.java
@@ -0,0 +1,23 @@
+package java1;
+
+import samplejava.Scanner;
+
+public class oddandeven {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number")
+		int value=sc.nextInt();
+		if(value%2==0) {
+		System.out.println("it is even");
+		}else
+		{
+			System.out.println("it is odd");
+		}
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
