66b39b132583d748c19cb11cc601dd81895e5c28
suresh379 <suresh234363@gmail.com>
2024-12-24 12:09:34 +0530

    Add files via upload

---
+++ multipletable.java
@@ -0,0 +1,29 @@
+package java1;
+
+import samplejava.Scanner;
+
+public class multipletable {
+
+	
+	    public static void main(String[] args) {
+	        Scanner obj=new Scanner(System.in);
+	        System.out.println("Enter a number");
+	        int n=obj.nextInt();
+	        int i=1;
+	        while(i<=10){
+	            System.out.println(n +"*" +i+ "="+ n*i);
+	            i++;
+	        }
+	       
+	    }
+	}		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
