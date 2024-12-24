313fdfd1b329c48f7194e280f9da79435faa044e
suresh379 <suresh234363@gmail.com>
2024-12-24 12:10:02 +0530

    Add files via upload

---
+++ sumevenodd.java
@@ -0,0 +1,29 @@
+package java1;
+
+public class sumevenodd {
+
+	public static void main(String[] args) {
+		int evenvalue=0;
+		int oddvalue=0;
+		int i=0;
+		while(i<=20) {
+			if(i%2==0) {
+				evenvalue+=i;
+			}else {
+				oddvalue+=i;
+			}
+		}
+		System.out.println("sum of even number is 0 to 20 :");
+		System.out.println("sum of  odd number is 0 to 20");
+		
+
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
